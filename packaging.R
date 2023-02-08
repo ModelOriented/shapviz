906626c44ecd90cce66595c64e4b272df9f7ce80
mayer79 <mayermichael79@gmail.com>
2023-02-08 17:25:32 +0100

    Slightly easier to read code in sv_importance()

--- packaging.R
+++ packaging.R
@@ -15,7 +15,7 @@ library(usethis)
 use_description(
   fields = list(
     Title = "SHAP Visualizations",
-    Version = "0.5.0",
+    Version = "0.5.1",
     Description = "Visualizations for SHAP (SHapley Additive exPlanations),
     such as waterfall plots, force plots, various types of importance plots,
     dependence plots, and interaction plots.
